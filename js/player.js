aacdfcc260847e28325b2786ac07599e0fc7cb38
Kevin Santiago Romero Roncancio <kevin.s.romero.r06@gmail.com>
2025-08-02 14:23:50 -0500

    Chore: Casi terminada la maquetacion

--- js/player.js
+++ js/player.js
@@ -6,18 +6,15 @@ const logoSENA = document.getElementById("logoSena");
 const boxLogoSena = document.querySelector(".senaLogo");
 const logoGov = document.getElementById("logoCobierno");
 const boxLogoGov = document.querySelector(".govLogo");
-var boxNavBar = document.querySelector('.boxNavBar')
-const escucharMas = document.querySelector('.boxEscucharMas')
-
-
+var boxNavBar = document.querySelector(".boxNavBar");
+const escucharMas = document.querySelector(".boxEscucharMas");
 
 window.addEventListener("scroll", () => {
   var boxLogos = document.querySelector(".boxSENAyGov");
   boxLogos.classList.toggle("abajo", window.scrollY > 50);
 
-  boxNavBar.classList.toggle('navAbajo', window.scrollY > 50);
-  escucharMas.classList.toggle('espacioArriba', window.scrollY > 50);
-
+  boxNavBar.classList.toggle("navAbajo", window.scrollY > 50);
+  escucharMas.classList.toggle("espacioArriba", window.scrollY > 50);
 
   if (window.scrollY > 50) {
     logoSENA.src = "./src/img/SENA_SENA VERDE.png";
@@ -27,7 +24,6 @@ window.addEventListener("scroll", () => {
     boxLogoSena.style.height = `60px`;
 
     boxLogoGov.style.width = `100px`;
-
   } else {
     logoSENA.src = "./src/img/SENA_SENA BLANCO.png";
     logoGov.src = "./src/img/SENA_TRABAJO.png";
@@ -37,7 +33,6 @@ window.addEventListener("scroll", () => {
     boxLogoGov.style.width = `150px`;
   }
 });
-
 
 media.addEventListener("change", (e) => updateNavbar(e));
 
@@ -82,3 +77,39 @@ function linkOferta() {
     "_blank"
   );
 }
+
+// footer
+// Función para verificar el tamaño de la ventana
+function insertarHTMLSiEsNecesario() {
+  const anchoVentana = window.innerWidth;
+  const contenedor = document.querySelector(".box-f-logos");
+  const originalContenedor = document.querySelector('.footer-logos')
+
+  if (anchoVentana < 600) {
+    contenedor.innerHTML = `
+          <div class="f-box-logo-sena">
+                    <img src="./src/img/SENA_SENA VERDE.png" alt="Logo SENA">
+                </div>
+                <div class="f-box-logos-icontec">
+                    <img src="http://www.sena.edu.co/Style%20Library/alayout/images/Icontec03_all.png" alt="Logos icontect">
+                </div>
+        `;
+        originalContenedor.innerHTML = ''
+  } else {
+    contenedor.innerHTML = ''
+    originalContenedor.innerHTML = `
+    <div class="f-box-logo-sena">
+                    <img src="./src/img/SENA_SENA VERDE.png" alt="Logo SENA">
+                </div>
+                <div class="f-box-logos-icontec">
+                    <img src="http://www.sena.edu.co/Style%20Library/alayout/images/Icontec03_all.png" alt="Logos icontect">
+                </div>
+    `
+  }
+}
+
+// Ejecutar la función al cargar la página
+insertarHTMLSiEsNecesario();
+
+// Escuchar cambios en el tamaño de la ventana
+window.addEventListener("resize", insertarHTMLSiEsNecesario);
